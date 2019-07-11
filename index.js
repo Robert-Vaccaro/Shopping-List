78da09cd624624a6ec8d793f2195c0bbb612c69d
Robert-Vaccaro <48065298+Robert-Vaccaro@users.noreply.github.com>
2019-07-11 14:10:12 -0700

    Update index.js  - indentations

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-$(function(){
-    $("#js-shopping-list-form").submit(function(event){
-      event.preventDefault();
-      const newItem = $('#shopping-list-entry').val();
+$(function() {
+    $("#js-shopping-list-form").submit(function(event) {
+        event.preventDefault();
+        const newItem = $('#shopping-list-entry').val();
 
-      $('#shopping-list-entry').val('');
-      
-      $("ul").append(
-        `<li><span class=\"shopping-item\">${newItem}</span><div class=\"shopping-item-controls\"><button class=\"shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>`
+        $('#shopping-list-entry').val('');
+
+        $("ul").append(
+            `<li><span class=\"shopping-item\">${newItem}</span><div class=\"shopping-item-controls\"><button class=\"shopping-item-toggle\"><span class=\"button-label\">check</span></button><button class=\"shopping-item-delete\"><span class=\"button-label\">delete</span></button></div></li>`
         );
-  });
-  
-  $('.shopping-item').removeClass('shopping-item__checked');
-//This line is to used to get rid of the line through milk that is there //intially.
+    });
 
-  $('.shopping-list').on('click','.shopping-item-delete',function(event){
-    $(this).closest('li').remove();
-  });
-  
-  $('.shopping-list').on('click','.shopping-item-toggle',function(event){
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-  });
+    $('.shopping-item').removeClass('shopping-item__checked');
+    //This line is to used to get rid of the line through milk that is there //intially.
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
 
 });
